9d338d94a11068f4bb34b3bc67bdc0aad6ff50fa
Gunvanth123 <guna.konathala123@gmail.com>
2025-05-25 21:06:44 +0530

    New projects for 1st sample

--- VIte_React_01/src/App.jsx
+++ VIte_React_01/src/App.jsx
@@ -1,14 +1,39 @@
-import New from "./New"
+import React, { useState } from "react";
+import New from "./New";
+import "./App.css";
 
 function App() {
-
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    if (count + 1 > 20) return;
+    setCount(count + 1);
+  };
+  const decrement = () => {
+    if (count - 1 < 0) return;
+    setCount(count - 1);
+  };
+  const reset = () => {
+    setCount(0);
+  };
   return (
     <>
-      <h1>Hello Vite Mowa</h1>
-      <h2>sfsfsf</h2>
+      <div className="App">
+        <h1 className="title">Counter App</h1>
+        <h2 className="counter">{count}</h2>
+        <button className="button" onClick={increment}>
+          Increment
+        </button>
+        <button className="button" onClick={decrement}>
+          Decrement
+        </button>
+        <button className="button" onClick={reset}>
+          Reset
+        </button>
+      </div>
+
       <New />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
