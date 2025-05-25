50b7c2d39e147ade3e4762b7e3ba3bbfe7ecf29a
Gunvanth123 <guna.konathala123@gmail.com>
2025-05-25 09:44:31 +0530

    New projects for 1st sample

---
+++ VIte_React_01/src/App.jsx
@@ -0,0 +1,14 @@
+import New from "./New"
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello Vite Mowa</h1>
+      <h2>sfsfsf</h2>
+      <New />
+    </>
+  )
+}
+
+export default App
---
+++ part_01/src/App.js
@@ -0,0 +1,11 @@
+import New from "./New";
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, World!</h1>
+      <New />
+    </div>
+  );
+}
+
+export default App;
